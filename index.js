969ba5a62c068f2a7798199c7b3270ddfecad939
Jesse Wenger <wengerj@gmail.com>
2021-06-07 16:21:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function submitData(newName, newEmail) {
     body: JSON.stringify(formData)
   };
 
-  function fetchUser() {
     return fetch(userUrl, configObj)
     .then(function(response) {
       return response.json();
@@ -29,26 +28,24 @@ function submitData(newName, newEmail) {
       console.log(error.message);
       renderErrors(error)
     });
-  }
+}
 
-  function renderUsers(newUser) {
-    const newUserId = newUser.id;
-    const ul = document.createElement('ul');
-    const li = document.createElement('li');
-    li.innerHTML = newUserId;
-    document.body.appendChild(ul);
-    ul.appendChild(li);
-    console.log(newUserId);
-  }
+function renderUsers(newUser) {
+  const newUserId = newUser.id;
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  li.innerHTML = newUserId;
+  document.body.appendChild(ul);
+  ul.appendChild(li);
+  console.log(newUserId);
+}
 
-  function renderErrors(error) {
-    const errorMsg = error.message
-    const ul = document.createElement('ul');
-    const li = document.createElement('li');
-    li.innerHTML = errorMsg;
-    document.body.appendChild(ul);
-    ul.appendChild(li);
-    console.log(errorMsg);
-  }
-  fetchUser();
+function renderErrors(error) {
+  const errorMsg = error.message
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  li.innerHTML = errorMsg;
+  document.body.appendChild(ul);
+  ul.appendChild(li);
+  console.log(errorMsg);
 }
