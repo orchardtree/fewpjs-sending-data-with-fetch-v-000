eb445ebb33c64941669a6e9e13fe128ec0f9228f
Jesse Wenger <wengerj@gmail.com>
2021-06-07 16:16:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function submitData(newName, newEmail) {
   };
 
   function fetchUser() {
-    return fetch(userUrl, configObj)
+    fetch(userUrl, configObj)
     .then(function(response) {
       return response.json();
     })
