dd40de207965f183ea49796df391689b7db4724e
Jesse Wenger <wengerj@gmail.com>
2021-06-07 15:10:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,22 +43,3 @@ function submitData(newName, newEmail) {
   }
   fetchUser();
 }
-
-/*
-fetchUsers(userUrl, configObj)
-function fetchUsers(userUrl, configObj) {
-  let users = fetch(userUrl, configObj)
-      .then(resp => resp.json())
-      .then(json => renderUsers(json.message));
-  return users;
-  }
-
-function renderUsers(users) {
-  const newUserId = newUser.id;
-  const ul = document.createElement('ul');
-  const li = document.createElement('li');
-  li.innerHTML = newUserId;
-  document.body.appendChild(ul);
-  ul.appendChild(li);
-  }
-*/
